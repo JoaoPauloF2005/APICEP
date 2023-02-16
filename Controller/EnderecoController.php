27606c2035cb852238a25d388cb07d32f5d2acda
joao <hypergamerfrr@gmail.com>
2023-02-16 16:01:31 -0300

    att

---
+++ Controller/EnderecoController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace ApiCep\Controller;
+
+use App\Model\{ EnderecoModel, CidadeModel };
+use Exception;
+
+class EnderecoController extends Controller
+{
+    public static function getCepByLogradouro() : void
+    {
+
+    }
+
+    public static function getLogradouroByBairroAndCidade ()  : void
+    {
+
+    }
+}
+ 
