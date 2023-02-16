0ad12c85d5ada83b87393a369e5ab08bb3f2e6f8
gaboliveiradev <g38789138@gmail.com>
2023-02-16 15:53:36 -0300

    att

--- index.php
+++ index.php
@@ -4,3 +4,12 @@
   include  'autoload.php';
   include  'rotas.php';
   
+  $cidades = ['Jaú', 'Bariri', 'Itapui', 'Dois Córregos'];
+
+ // var_dump($cidades);
+
+ use ApiCep\Controller;
+
+ include 'Controller/Controller.php';
+
+ \ApiCep\Controller\Controller::getResponseAsJSON($cidades);
