0560befbd6973e6ea89d1ff6bf506014f78d9ee1
joao <hypergamerfrr@gmail.com>
2023-03-02 16:09:57 -0300

    att

---
+++ DAO/DAO.php
@@ -0,0 +1,33 @@
+<?php
+
+use Exception;
+use \PDO;
+USE PDOException;           
+
+abstract class DAO extends PDO
+{
+   protected $conexao;
+
+    public function __construct()
+    {
+        try
+        {
+           $options = [
+            PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+            PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf-8'
+        
+           ];
+
+           $dsn = "mysql:host=" . $_ENV['db']['host'] . "dbname=" .  $_ENV['db']['database'];
+    
+           $this->conexao = new PDO($dsn, $_ENV['db']['user'], $_ENV['db']['pass'], $options);
+        }
+
+        catch(PDOException $e)
+        {
+            throw new Exception("Ocorreu um erro ao tentar conectar ao MySQL", 0, $e);
+        }
+    
+    }
+    
+}
